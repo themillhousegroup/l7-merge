b157399cb47e5fc3dcb21565bbcb93519e189731
John Marshall <john@themillhousegroup.com>
2014-10-16 07:39:14 +1100

    Fixed backwards detection logic

--- src/main/scala/com/themillhousegroup/l7/Automerge.scala
+++ src/main/scala/com/themillhousegroup/l7/Automerge.scala
@@ -37,7 +37,7 @@ class Automerge(val existingDir: File, val newerDir: File) extends LazyLogging {
 
   def merge(dryRun: Boolean = false): Try[Seq[File]] = {
     val maybeExistingFiles = xmlFilesIn(existingDir)
-    val maybeNewerFiles = xmlFilesIn(existingDir)
+    val maybeNewerFiles = xmlFilesIn(newerDir)
 
     for {
       existingFiles <- maybeExistingFiles
@@ -47,13 +47,14 @@ class Automerge(val existingDir: File, val newerDir: File) extends LazyLogging {
   }
 
   def process(existingFiles: Seq[File], newerFiles: Seq[File]): Try[Seq[File]] = {
+    println(s"existing $existingFiles; newer: $newerFiles")
     if (newerFiles.size < existingFiles.size) {
       failWith("I'm not clever enough to merge if #newer < #older. Sorry")
     } else if (newerFiles.size > existingFiles.size) {
-      val diffs = existingFiles.diff(newerFiles)
+      val diffs = newerFiles.map(_.getName).diff(existingFiles.map(_.getName))
       logger.info(s"The following files seem to be new in $newerDir:")
-      logger.info(s"${diffs.map(_.getName).mkString(",")}")
-      Success(diffs)
+      logger.info(s"${diffs.mkString(",")}")
+      Success(newerFiles.filter(f => diffs.contains(f.getName)))
     } else {
       Success(Nil)
     }
