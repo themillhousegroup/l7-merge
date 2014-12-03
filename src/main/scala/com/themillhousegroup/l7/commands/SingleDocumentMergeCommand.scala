4f02c7e19615f00c5d6a0da04f3418da62646577
John Marshall <john.marshall2@auspost.com.au>
2014-12-03 11:22:24 +1100

    Render options 'automatically'

--- src/main/scala/com/themillhousegroup/l7/commands/SingleDocumentMergeCommand.scala
+++ src/main/scala/com/themillhousegroup/l7/commands/SingleDocumentMergeCommand.scala
@@ -3,27 +3,28 @@ package com.themillhousegroup.l7.commands
 import java.io.File
 import com.themillhousegroup.l7.{ SingleDocumentOperations, HierarchyBuilder }
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.commons.lang3.StringUtils
 
 object SingleDocumentMergeCommand extends Command("merge-one") with LazyLogging {
 
-  val expectedArgs =
-    "<file1> <file2> [merge-options] to merge the contents of file2 into file1\n" +
-      "  Where [merge-options] are:\n" +
-      "    --force              Merge even if the files seem very different\n" +
-      "    --only-structural    Retain the 'old' GUID references if any\n" +
-      "    --version-aware      Use the version numbers in the files to work out newer vs older\n"
+  lazy val expectedArgs =
+    s"<file1> <file2> [merge-options] to merge the contents of file2 into file1\nWhere [merge-options] are:\n$renderedOptions"
 
   object Options {
     val forceMerge = "--force"
     val onlyStructural = "--only-structural"
     val versionAware = "--version-aware"
+    val retainOldVersion = "--retain-old-version"
   }
 
-  val options = Map(
+  lazy val options = Map(
     Options.forceMerge -> "Merge even if the files seem 'too different'",
     Options.onlyStructural -> "Retain references to old GUIDs - i.e. changes are structural to this file",
-    Options.versionAware -> "Inspect for version numbers and use those to determine the older/newer file"
+    Options.versionAware -> "Inspect for version numbers and use those to determine the older/newer file",
+    Options.retainOldVersion -> "Keep the 'version' from the 'older' file (minimises diffs)"
   )
+
+  lazy val renderedOptions = options.map { case (k, v) => StringUtils.rightPad(k, 24) + v }.mkString("\n")
 
   def runWith(args: Seq[String], options: Seq[String]) = {
     if (args.size < 2) {
