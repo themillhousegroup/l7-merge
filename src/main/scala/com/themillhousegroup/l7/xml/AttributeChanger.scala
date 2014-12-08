da40160bbac3e93189ac77c3916e5c13e1cd50c9
John Marshall <john.marshall2@auspost.com.au>
2014-12-09 10:13:16 +1100

    Tests for AttributeChanger in anticipation of refactor

--- src/main/scala/com/themillhousegroup/l7/xml/AttributeChanger.scala
+++ src/main/scala/com/themillhousegroup/l7/xml/AttributeChanger.scala
@@ -6,10 +6,11 @@ import scala.xml.transform.{ RuleTransformer, RewriteRule }
 object AttributeChanger {
 
   def convert(doc: Node, label: Option[String], attribName: String, newValue: String): Elem = {
+
     val rewrite = new RewriteRule {
 
       def innerTransform(n: Node): Node = n match {
-        case elem @ Elem(_, label, atts, _, child @ _*) => {
+        case elem @ Elem(_, lbl, atts, _, child @ _*) if label.isEmpty || label.contains(lbl) => {
           val maybeAttrib = Option(atts(attribName))
           maybeAttrib.map { a =>
             elem.asInstanceOf[Elem] % Attribute(None, attribName, Text(newValue), Null) copy (child = child map innerTransform)
--- src/test/scala/com/themillhousegroup/l7/xml/AttributeChangerSpec.scala
+++ src/test/scala/com/themillhousegroup/l7/xml/AttributeChangerSpec.scala
@@ -1,5 +1,37 @@
 package com.themillhousegroup.l7.xml
 
-class AttributeChangerSpec {
+import org.specs2.mutable.Specification
 
+class AttributeChangerSpec extends Specification {
+
+  val sourceElem =
+    <parent>
+      <children size="3">
+        <child id="first">Original First</child>
+        <child id="second">Original Second</child>
+        <child id="third">Original Third</child>
+      </children>
+      <name id="blah">Original Name</name>
+    </parent>
+
+  "AttributeChanger" should {
+
+    "be able to perform blanket replacement of attributes" in {
+
+      val result = AttributeChanger.convert(sourceElem, None, "id", "foo")
+
+      val ids = result \\ "@id"
+
+      ids.filter(_.text == "foo") must haveSize(4)
+    }
+
+    "be able to perform targeted replacement of attributes" in {
+
+      val result = AttributeChanger.convert(sourceElem, Some("child"), "id", "foo")
+
+      val ids = result \\ "@id"
+
+      ids.filter(_.text == "foo") must haveSize(3)
+    }
+  }
 }
